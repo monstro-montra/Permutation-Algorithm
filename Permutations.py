1da54492ac285f7b55f07c431c1af01890c2ede4
Alex7861 <alexjavier1337@gmail.com>
2023-08-16 05:56:32 -0400

    permutation algorithm is complete.

--- Permutations.py
+++ Permutations.py
@@ -1,32 +1,31 @@
 def main():
-    list = [1, 2, 3, 4, 5]  # initiate the list
-    print_array(list)
-    print(permute(list))
+    numbers = [6, 7, 4, 2]  # initiate the numbers
+    print_array(numbers)
+    print
+    for perm in permute(numbers):
+        print(perm)
 
-def permute(list):
-    size = len(list)
-    if len(list) == 0:
+def permute(numbers):
+    if len(numbers) == 0:  # account for base case. return nothing if there are no elements in the list.
         return []
         
-    if len(list) == 1:
-        return [list]
+    if len(numbers) == 1:  # account for base case. return array if the length is only one.
+        return [numbers]
     
-    result = []
-    for i in range(len(list)):
-        c = list[i]
-        #isolate i in remaining list 
-        remainingList = list[:i] + list[i + 1:]  # remainingList = list from beginning to i (except i) plus the list[i+1] to the end of the list. 
+    result = [] # starts 
+    for i in range(len(numbers)): # start a for loop that goes from i to the length of numbers array. (in this case it is 4.)
+        c = numbers[i] 
+        #isolate i in remaining numbers 
+        remainingList = numbers[:i] + numbers[i + 1:]  # remainingList = numbers from beginning to i (except i) plus the numbers[i+1] to the end of the numbers. 
         for permutation in permute(remainingList):
             result.append([c] + permutation)
-        return result
-
-def swap(list, i, j): # swap method
-    list[i], list[j] = list[j], list[i]
+            
+    return result
 
 
-def print_array(list):
-    for i in range(len(list)):
-        print(list[i], end=" ")
+def print_array(numbers):
+    for i in range(len(numbers)):
+        print(numbers[i], end=" ")
 
 
 if __name__ == "__main__":
